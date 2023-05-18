c19c349f18945efa52c13e7bb6b8512ddffc87b9
Felipe Ricardo <85915186+felipricardo@users.noreply.github.com>
2023-05-18 13:30:09 -0300

    Create matriz1.py

---
+++ matriz1.py
@@ -0,0 +1,18 @@
+# Criando a matriz 3x5 e preenchendo os valores
+matriz = []
+for i in range(3):
+    linha = []
+    for j in range(5):
+        elemento = int(input(f"Digite o valor para a posição ({i+1}, {j+1}): "))
+        linha += [elemento]
+    matriz += [linha]
+
+# Calculando a quantidade de elementos entre 15 e 20
+contador = 0
+for linha in matriz:
+    for elemento in linha:
+        if 15 <= elemento <= 20:
+            contador += 1
+
+# Exibindo o resultado
+print("Quantidade de elementos entre 15 e 20:", contador)
